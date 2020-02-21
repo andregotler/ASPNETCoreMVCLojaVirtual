f83d98e86467b6a745b4282c69efceed6b1e6a10
andregotler <andresgotler@hotmail.com>
2020-02-21 01:02:57 -0300

    Loja Virtual

--- Seção 02/PrimeiroProjeto/PrimeiroProjeto/Models/Produto.cs
+++ Seção 02/PrimeiroProjeto/PrimeiroProjeto/Models/Produto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,5 +12,24 @@ namespace PrimeiroProjeto.Models
         public string Nome { get; set; }
         public string Descricao { get; set; }
         public decimal Valor { get; set; }
+        public int Quantidade { get; set; }
+        
+        //frete - correio
+
+        public double Peso { get; set; }
+        public int Largura { get; set; }
+        public int Altura { get; set; }
+        public int Comprimento { get; set; }
+            
+        //banco de dados - relacionamento entre tabela
+        public int CategoriaId { get; set; }
+
+        //Programação orientada a objetos
+        [ForeignKey("CategoriaId")]
+        public Categoria Categoria { get; set; }
+
+        /*EF - ORM - Biblioteca unir - Banco de dados e POO. (ORM - Mapeamento de Objetos <-> Relacionamento)
+         * Fluent API - Attributes*/
+
     }
 }
