a669592a0b2edeab4e4f1b10aab78abd5391cdcd
andregotler <andresgotler@hotmail.com>
2020-03-04 15:49:48 -0300

    Loja Virtual

--- Seção 02/PrimeiroProjeto/PrimeiroProjeto/Areas/Colaborador/Controllers/ProdutoController.cs
+++ Seção 02/PrimeiroProjeto/PrimeiroProjeto/Areas/Colaborador/Controllers/ProdutoController.cs
@@ -3,28 +3,78 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using PrimeiroProjeto.Libraries.Filtro;
+using PrimeiroProjeto.Libraries.Lang;
+using PrimeiroProjeto.Models;
 using PrimeiroProjeto.Repositories;
+using PrimeiroProjeto.Repositories.Contracts;
 
-namespace PrimeiroProjeto.Areas.Colaborador.Controllers
-{
+namespace PrimeiroProjeto.Areas.Colaborador.Controllers {
     [Area("Colaborador")]
     [ColaboradorAutorizacao]
-    public class ProdutoController : Controller
-    {
+    public class ProdutoController : Controller {
         private IProdutoRepository _produtoRepository;
-        public ProdutoController(IProdutoRepository produtoRepository) {
+        private ICategoriaRepository _categoriaRepository;
+
+        public ProdutoController(IProdutoRepository produtoRepository, ICategoriaRepository categoriaRepository) {
             _produtoRepository = produtoRepository;
+            _categoriaRepository = categoriaRepository;
         }
 
-        public IActionResult Index(int? pagina, string pesquisa)
-        {
+        public IActionResult Index(int? pagina, string pesquisa) {
             var produtos = _produtoRepository.ObterTodosProdutos(pagina, pesquisa);
             return View(produtos);
         }
         [HttpGet]
+        [ValidateHttpReferer]
         public IActionResult Cadastrar() {
+            ViewBag.Categorias = _categoriaRepository.ObterTodasCategorias().Select(a => new SelectListItem(a.Nome, a.id.ToString()));
             return View();
         }
+        [HttpPost]
+        public IActionResult Cadastrar (Produto produto) {
+            if(ModelState.IsValid) {
+
+                _produtoRepository.Cadastrar(produto);
+                TempData["MSG_S"] = Mensagem.MSG_S001;
+                return RedirectToAction(nameof(Index));
+
+            }
+            ViewBag.Categorias = _categoriaRepository.ObterTodasCategorias().Select(a => new SelectListItem(a.Nome, a.id.ToString()));
+
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Atualizar(int id) {
+
+            ViewBag.Categorias = _categoriaRepository.ObterTodasCategorias().Select(a => new SelectListItem(a.Nome, a.id.ToString()));
+            Produto produto = _produtoRepository.ObterProduto(id);
+            return View(produto);
+        }
+
+        [HttpPost]
+        public IActionResult Atualizar(Produto produto, int id) {
+
+            if (ModelState.IsValid) {
+                _produtoRepository.Atualizar(produto);
+
+                TempData["MSG_S"] = Mensagem.MSG_S001;
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            ViewBag.Categorias = _categoriaRepository.ObterTodasCategorias().Select(a => new SelectListItem(a.Nome, a.id.ToString()));
+            return View();
+        }
+
+        [HttpGet]
+        [ValidateHttpReferer]
+        public IActionResult Excluir(int id) {
+            _produtoRepository.Excluir(id);
+            TempData["MSG_S"] = Mensagem.MSG_S002;
+            return RedirectToAction(nameof(Index));
+        }
+
     }
 }
--- Seção 02/PrimeiroProjeto/PrimeiroProjeto/Models/Produto.cs
+++ Seção 02/PrimeiroProjeto/PrimeiroProjeto/Models/Produto.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using PrimeiroProjeto.Libraries.Lang;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,19 +11,43 @@ namespace PrimeiroProjeto.Models
     public class Produto
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_E001")]
         public string Nome { get; set; }
+
+        [Display(Name = "Descrição")]
+        [Required(ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_E001")]
         public string Descricao { get; set; }
+
+        [Display(Name = "Preço")]
+        [Required(ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_E001")]
         public decimal Valor { get; set; }
+
+        [Required(ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_E001")]
+        [Range(0,10000, ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_E006")]
         public int Quantidade { get; set; }
-        
+
         //frete - correio
 
+        [Range(0.001, 30, ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_E006")]
+        [Required(ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_E001")]
         public double Peso { get; set; }
+
+        [Range(11, 105, ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_E006")]
+        [Required(ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_E001")]
         public int Largura { get; set; }
+
+        [Range(2, 105, ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_E006")]
+        [Required(ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_E001")]
         public int Altura { get; set; }
+
+        [Range(16, 105, ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_E006")]
+        [Required(ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_E001")]
         public int Comprimento { get; set; }
-            
+
         //banco de dados - relacionamento entre tabela
+        [Display(Name = "Categoria")]
+        [Required(ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_E001")]
         public int CategoriaId { get; set; }
 
         //Programação orientada a objetos
@@ -30,7 +56,6 @@ namespace PrimeiroProjeto.Models
 
         /*EF - ORM - Biblioteca unir - Banco de dados e POO. (ORM - Mapeamento de Objetos <-> Relacionamento)
          * Fluent API - Attributes*/
-
         public virtual ICollection<Imagem> Imagens { get; set; }
     }
 }
