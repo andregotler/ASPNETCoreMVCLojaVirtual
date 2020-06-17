e8bc10bceb813b6fd2bb81e783a47791b37b349b
André Gotler <59943689+andregotler@users.noreply.github.com>
2020-06-16 21:11:19 -0300

    Loja Virtual

---
+++ Seção 02/PrimeiroProjeto/PrimeiroProjeto/Libraries/Component/MenuViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PrimeiroProjeto.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrimeiroProjeto.Libraries.Component
+{
+    public class MenuViewComponent : ViewComponent
+    {
+
+        private ICategoriaRepository _categoriaRepository;
+        public MenuViewComponent(ICategoriaRepository categoriaRepository) {
+            _categoriaRepository = categoriaRepository;
+        }
+        public async Task<IViewComponentResult> InvokeAsync() {
+            var ListaCategoria = _categoriaRepository.ObterTodasCategorias().ToList();
+            return View();
+        }
+    }
+}
